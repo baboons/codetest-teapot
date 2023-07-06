22c476cda4e0e4d8e7dc91b780c010550f58ff50
johan <johan@baboons.se>
2023-07-06 16:16:20 +0200

    add: prettier

--- teapot.mjs
+++ teapot.mjs
@@ -10,43 +10,42 @@ async function loadTeapotGeometry() {
   const teapotResponse = await fetch("/teapot.obj");
   const teapotText = await teapotResponse.text();
 
-  const vertices = []
-  const indexes = []
-  const normals = []
-
+  const vertices = [];
+  const indexes = [];
+  const normals = [];
 
   // Parse the obj file line by line
   for (const line of teapotText.split("\n")) {
-      const items = line.trim().split(" ")
+    const items = line.trim().split(" ");
 
-      switch (items[0]) {
-        case "v":
-          vertices.push(...items.slice(1, 4).map((v) => parseFloat(v)/5));
-          break;
-        case "vn":
-          normals.push(...items.slice(1, 4).map((v) => parseFloat(v)));
-          break
-        case "f":
-          const ndx = items.slice(1).map((v) => {
-            const indices = v.split("/").map((index) => parseInt(index, 10) - 1);
-            return indices[0];
-          });
-          switch (ndx.length) {
-            case 3:
-              indexes.push(ndx[0], ndx[1], ndx[2]);
-              break;
-            case 4:
-              indexes.push(ndx[0], ndx[1], ndx[2], ndx[2], ndx[3], ndx[0]);
-              break;
-          }
-          break;
-      }
+    switch (items[0]) {
+      case "v":
+        vertices.push(...items.slice(1, 4).map((v) => parseFloat(v) / 5));
+        break;
+      case "vn":
+        normals.push(...items.slice(1, 4).map((v) => parseFloat(v)));
+        break;
+      case "f":
+        const ndx = items.slice(1).map((v) => {
+          const indices = v.split("/").map((index) => parseInt(index, 10) - 1);
+          return indices[0];
+        });
+        switch (ndx.length) {
+          case 3:
+            indexes.push(ndx[0], ndx[1], ndx[2]);
+            break;
+          case 4:
+            indexes.push(ndx[0], ndx[1], ndx[2], ndx[2], ndx[3], ndx[0]);
+            break;
+        }
+        break;
+    }
   }
 
   return {
     indexes: new Uint16Array(indexes),
     vertices: new Float32Array(vertices),
-    normals: new Float32Array(normals)
+    normals: new Float32Array(normals),
   };
 }
 
@@ -59,7 +58,9 @@ function setupShaderProgram(context) {
   const vertexShader = context.createShader(context.VERTEX_SHADER);
   const fragmentShader = context.createShader(context.FRAGMENT_SHADER);
 
-  context.shaderSource(vertexShader, `
+  context.shaderSource(
+    vertexShader,
+    `
     attribute vec3 position;
     attribute vec3 normal;
     uniform mat4 modelViewMatrix;
@@ -68,8 +69,11 @@ function setupShaderProgram(context) {
       gl_Position = modelViewMatrix * vec4(position, 1);
       vNormal = normal;
     }
-  `);
-  context.shaderSource(fragmentShader, `
+  `,
+  );
+  context.shaderSource(
+    fragmentShader,
+    `
     precision mediump float;
     varying vec3 vNormal;
     void main() {
@@ -103,7 +107,8 @@ function setupShaderProgram(context) {
       gl_FragColor = vec4(color, 1.0);
 
     }
-  `);
+  `,
+  );
 
   context.compileShader(vertexShader);
   context.compileShader(fragmentShader);
@@ -129,7 +134,11 @@ async function renderTeapot() {
   // Bind indexes to ELEMENT_ARRAY_BUFFER
   const index = context.createBuffer();
   context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, index);
-  context.bufferData(context.ELEMENT_ARRAY_BUFFER, teapotGeometry.indexes, context.STATIC_DRAW);
+  context.bufferData(
+    context.ELEMENT_ARRAY_BUFFER,
+    teapotGeometry.indexes,
+    context.STATIC_DRAW,
+  );
 
   // Use the red shader program
   const program = setupShaderProgram(context);
@@ -138,7 +147,11 @@ async function renderTeapot() {
   // Bind vertices to ARRAY_BUFFER
   const position = context.createBuffer();
   context.bindBuffer(context.ARRAY_BUFFER, position);
-  context.bufferData(context.ARRAY_BUFFER, teapotGeometry.vertices, context.STATIC_DRAW);
+  context.bufferData(
+    context.ARRAY_BUFFER,
+    teapotGeometry.vertices,
+    context.STATIC_DRAW,
+  );
 
   // Bind position to it shader attribute
   const positionLocation = context.getAttribLocation(program, "position");
@@ -148,65 +161,77 @@ async function renderTeapot() {
   // Bind normal to ARRAY_BUFFER
   const normal = context.createBuffer();
   context.bindBuffer(context.ARRAY_BUFFER, normal);
-  context.bufferData(context.ARRAY_BUFFER, teapotGeometry.normals, context.STATIC_DRAW);
+  context.bufferData(
+    context.ARRAY_BUFFER,
+    teapotGeometry.normals,
+    context.STATIC_DRAW,
+  );
 
   // Bind normal to it shader attribute
   const normalLocation = context.getAttribLocation(program, "normal");
   context.enableVertexAttribArray(normalLocation);
   context.vertexAttribPointer(normalLocation, 3, context.FLOAT, false, 0, 0);
 
-
-  let firstFrame = performance.now();
-
-  const startDragging = (event) => {
-      isDragging = true;
-      previousMousePosition = [event.clientX, event.clientY];
-  }
-
-  const stopDragging = (event) => {
-      isDragging = false;
-  }
-
-  const drag = (event) => {
-      console.log('drag', isDragging)
-      if (isDragging) {
-          const deltaX = event.clientX - previousMousePosition[0];
-          const deltaY = event.clientY - previousMousePosition[1];
-          rotationY += deltaX * 0.01;
-          rotationX += deltaY * 0.01;
-          previousMousePosition = [event.clientX, event.clientY];
-          console.log(rotationX, rotationY)
-      }
-  }
-
-  let rotationX = 0;
-  let rotationY = 0;
-
-  // Setup listeners for mouse events
-  canvas.addEventListener('mousedown', startDragging);
-  canvas.addEventListener('mouseup', stopDragging);
-  canvas.addEventListener('mouseout', stopDragging);
-  canvas.addEventListener('mousemove', drag);
-
   let isDragging = false;
   let previousMousePosition;
+  let rotation = 0;
 
+  const startDragging = (event) => {
+    isDragging = true;
+    previousMousePosition = [event.clientX, event.clientY];
+  };
+
+  const stopDragging = () => {
+    isDragging = false;
+  };
+
+  const drag = (event) => {
+    if (isDragging) {
+      rotation = event.clientX - previousMousePosition[0];
+      previousMousePosition = [event.clientX, event.clientY];
+    }
+  };
+
+  // Setup listeners for mouse events
+  canvas.addEventListener("mousedown", startDragging);
+  canvas.addEventListener("mouseup", stopDragging);
+  canvas.addEventListener("mouseout", stopDragging);
+  canvas.addEventListener("mousemove", drag);
 
   const renderLoop = () => {
-    const delta = performance.now() - firstFrame;
-
     // Set a rotating model view matrix
-    const modelViewMatrixLocation = context.getUniformLocation(program, "modelViewMatrix");
-    const rotation = delta % 10000 / 10000 * Math.PI * 2;
-    context.uniformMatrix4fv(modelViewMatrixLocation, false, new Float32Array([
-      Math.cos(rotation), 0, Math.sin(rotation), 0,
-      0, 1, 0, 0,
-      -Math.sin(rotation), 0, Math.cos(rotation), 0,
-      0, 0, 0, 1
-    ]));
+    const modelViewMatrixLocation = context.getUniformLocation(
+      program,
+      "modelViewMatrix",
+    );
+    const rotationMatrix = new Float32Array([
+      Math.cos(rotation),
+      0,
+      Math.sin(rotation),
+      0,
+      0,
+      1,
+      0,
+      0,
+      -Math.sin(rotation),
+      0,
+      Math.cos(rotation),
+      0,
+      0,
+      0,
+      0,
+      1,
+    ]);
+
+    context.uniformMatrix4fv(modelViewMatrixLocation, false, rotationMatrix);
 
     // Render the teapot
-    context.drawElements(context.TRIANGLES, teapotGeometry.indexes.length, context.UNSIGNED_SHORT, 0);
+    context.drawElements(
+      context.TRIANGLES,
+      teapotGeometry.indexes.length,
+      context.UNSIGNED_SHORT,
+      0,
+    );
     context.flush();
 
     // Request another frame
